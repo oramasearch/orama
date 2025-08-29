b4045aee87b4b98c03220632e91faca8e323c30d
Michele Riva <ciao@micheleriva.it>
2025-08-28 17:49:31 -0700

    refactor: removes docs (#966)

--- packages/docs/config/index.ts
+++
@@ -1,6 +0,0 @@
-import openSourceMenu from './menu/open-source'
-import oramaCloudMenu from './menu/orama-cloud'
-import cookieConsentConfig from './cookies'
-import head from './head'
-
-export { openSourceMenu, oramaCloudMenu, cookieConsentConfig, head }
